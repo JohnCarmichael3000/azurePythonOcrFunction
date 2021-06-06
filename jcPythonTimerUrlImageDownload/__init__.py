f148832d63c58c9b7dbaac8229f5a9b7a0969a54
JohnCarmichael3000 <johncarmichael@hotmail.com>
2021-06-05 17:51:36 -0700

    update comments

--- jcPythonTimerUrlImageDownload/__init__.py
+++ jcPythonTimerUrlImageDownload/__init__.py
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------
-# JOHN C - jcPythonTimerUrlImageDownload
+# jcPythonTimerUrlImageDownload
 #
 # THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
 # EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
